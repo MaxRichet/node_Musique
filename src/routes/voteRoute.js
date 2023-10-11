6d6a3f74af8ad151d73c15d1209eba05740da551
MaxRichet <max.richet@my-digital-school.org>
2023-10-11 17:00:19 +0200

    Syntax fix on name route

--- src/routes/voteRoute.js
+++ src/routes/voteRoute.js
@@ -4,16 +4,16 @@ const router = express.Router();
 const voteController = require('../controllers/voteController');
 
 // /posts
-// router
-//     .route('/votes/:id_vote/music')
-//     .get(voteController.listAllVote)
-//     .post(voteController.createAVotes);
+router
+    .route('/votes/:id_vote/music')
+    .get(voteController.listAllVotes)
+    .post(voteController.createAVote);
 
-// // /posts/:id_post
-// router
-//     .route('/comments/:id_com')
-//     .delete(voteController.deleteAVotes)
-//     .put(voteController.updateAVotes)
-//     .get(voteController.getAVotes);
+// /posts/:id_post
+router
+    .route('/musics/:id_vote')
+    .delete(voteController.deleteAVote)
+    .put(voteController.updateAVote)
+    .get(voteController.getAVote);
 
 module.exports = router;
